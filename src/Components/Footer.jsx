751faf3e4987cb36bceff413bcae4f45ea1eac99
mokadde <mokaddes.ru2000@gmail.com>
2024-12-06 20:21:34 +0600

    commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <div className="bg-black text-white flex justify-center items-start gap-2 p-5">
+        <p>© 2021, All rights reserved.</p>
+        <p>Designed by <a href="https://www.linkedin.com/in/abhishek-singh-0b4b7b1b0/" target="_blank" rel="noreferrer">Abhishek Singh</a></p>
+    </div>
+    </>
+  )
+}
+
+export default Footer
